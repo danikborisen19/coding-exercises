8c553a7f3e2a21df6571dbd58f6a41f66277a298
Daniel Borisenko <danikborisen19@gmail.com>
2024-11-08 13:39:22 -0500

    Created multiple objects of class "Car"

--- SolutionForMyProjects/FirstProject/Car.cs
+++ SolutionForMyProjects/FirstProject/Car.cs
@@ -1,22 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace FirstProject
 {
-
-
-
     class Car
     {
         string color = "red";
         static void Main(string[] args)
         {
-            Car myObj = new Car();
-            Console.WriteLine(myObj.color);
+            Car myObj1 = new Car();
+            Car myObj2 = new Car();
+            Console.WriteLine(myObj1.color);
+            Console.WriteLine(myObj2.color);
         }
     }
-
 }
--- SolutionForMyProjects/FirstProject/Program.cs
+++ SolutionForMyProjects/FirstProject/Program.cs
@@ -1,35 +1,33 @@
-﻿namespace FirstProject
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {    
-            Console.WriteLine(IsPalindrome("alla"));
-           
-      
+﻿namespace FirstProject;
 
-        }
-        public static bool IsPalindrome(string inputString)
-        {
-            inputString = inputString.ToLower();
-            inputString = new string(inputString.Where(char.IsLetterOrDigit).ToArray());
-            int left = 0;
-            int right = inputString.Length - 1;
+//internal class Program
+//{
 
-            while (left < right)
-            {
-                if (inputString[left] != inputString[right])
-                {
-                    return false;
-                }
-                left++;
-                right--;
-            }
-            return true;
-        }
-    
-    }
-}
+    //static void Main(string[] args)
+    //{
+    //    Console.WriteLine(IsPalindrome("alla"));
 
 
+
+    //}
+//    public static bool IsPalindrome(string inputString)
+//    {
+//        inputString = inputString.ToLower();
+//        inputString = new string(inputString.Where(char.IsLetterOrDigit).ToArray());
+//        int left = 0;
+//        int right = inputString.Length - 1;
+
+//        while (left < right)
+//        {
+//            if (inputString[left] != inputString[right])
+//            {
+//                return false;
+//            }
+//            left++;
+//            right--;
+//        }
+//        return true;
+//    }
+
+//}
 
