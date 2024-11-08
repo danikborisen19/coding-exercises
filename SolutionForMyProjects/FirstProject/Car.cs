dd9a8758f55cfe157de512564a86eb1eed261180
Daniel Borisenko <danikborisen19@gmail.com>
2024-11-08 13:04:18 -0500

    Wrote code for car object

---
+++ SolutionForMyProjects/FirstProject/Car.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstProject
+{
+
+
+
+    class Car
+    {
+        string color = "red";
+        static void Main(string[] args)
+        {
+            Car myObj = new Car();
+            Console.WriteLine(myObj.color);
+        }
+    }
+
+}
