f1938aaf47d2b818fe4f0c61ba637a84b79f5662
Daniel Borisenko <danikborisen19@gmail.com>
2024-11-12 20:51:22 -0500

    Created multiple objects of one class

--- SolutionForMyProjects/FirstProject/Car.cs
+++ SolutionForMyProjects/FirstProject/Car.cs
@@ -7,27 +7,32 @@ using System.Threading.Tasks;
 
 namespace FirstProject
 {
-    class MyClass
+    class Car
     {
-
-        string color = "red";
-        int maxSpeed = 200;
-
-        public void fullThrottle()
+        public string model;
+        public string color;
+        public int year;
+    }
+    class MyClass
         {
-            Console.WriteLine("The car is going as fast as it can!");
-        }
-        class Program
+        static void Main(string[] args)
         {
-            static void Main(string[] args)
-            {
-                MyClass myCar = new MyClass();
-                myCar.fullThrottle();
-            }
+            Car Ford = new Car();
+            Ford.model = "Mustang";
+            Ford.color = "red";
+            Ford.year = 1969;
+
+            Car Opel = new Car();
+            Opel.model = "Astra";
+            Opel.color = "white";
+            Opel.year = 2005;
+
+            Console.WriteLine(Ford.model);
+            Console.WriteLine(Opel.model);
         }
     }
 }
-        
+
 
 
 
